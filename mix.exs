66dbd76e81302b91012724736837720813a1fa75
szTheory <szTheory@users.noreply.github.com>
2026-05-18 23:07:38 +0200

    chore(23-01): add tiktoken dependency

--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule Scoria.MixProject do
       {:phoenix_live_view, "~> 1.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_ecto, "~> 4.4"},
+      {:tiktoken, "~> 0.4.2"},
       {:floki, ">= 0.30.0", only: :test},
       {:lazy_html, ">= 0.1.0", only: :test}
     ]
